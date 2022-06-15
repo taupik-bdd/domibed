6dbd42aaa302f6aa875a3194222039d22209d824
bayurzki <49741767+bayurzki@users.noreply.github.com>
2022-06-15 18:16:47 +0700

    wp-config-sv

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'web_domibed_2' );
+define( 'DB_NAME', 'u6065760_domibed' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u6065760_domibed' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Lyn$B#V[UH^z' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
